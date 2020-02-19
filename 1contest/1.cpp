8a40892915d5b00f86df73f5d00c8fbefda0ecc6
Alex <54078844+AZelentsov343@users.noreply.github.com>
2020-02-19 11:28:20 +0300

    Update 1.cpp

--- 1contest/1.cpp
+++ 1contest/1.cpp
@@ -1,2 +1,17 @@
+class Sum {
+public:
+    Sum(int a, int b) {
+        sum = a + b;
+    }
 
+    Sum(const Sum s, int c) {
+        sum = s.get() + c;
+    }
 
+    int get() const {
+        return sum;
+    }
+
+private:
+    int sum;
+};
