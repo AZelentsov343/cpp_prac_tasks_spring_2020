a02adb90ed0d693f52f0108dcf28cf9ef8cde101
Alex <54078844+AZelentsov343@users.noreply.github.com>
2020-05-16 00:17:25 +0300

    Update 2.cpp

--- 4contest/2.cpp
+++ 4contest/2.cpp
@@ -6,4 +6,13 @@
 Стандартными алгоритмами пользоваться запрещено.
 
 
-
+template <typename container_type, typename P>
+container_type myfilter(const container_type& container, P pred) {
+    container_type result{};
+    for (auto const &el : container) {
+        if (pred(el)) {
+            result.insert(result.end(), el);
+        }
+    }
+    return result;
+}
