6f48dc566ce2d592f02017eea3e1c53655a46355
Alex <54078844+AZelentsov343@users.noreply.github.com>
2020-05-16 00:19:43 +0300

    Update 2.cpp

--- 5contest/2.cpp
+++ 5contest/2.cpp
@@ -14,3 +14,70 @@
 За ошибки компиляции будут сниматься баллы.
 
 
+#include <sstream>
+#include <string>
+#include <cmath>
+
+class Rectangle : public Figure {
+public:
+
+    Rectangle(double height, double width) : height(height), width(width) {};
+
+
+    double get_square() const final {
+        return height * width;
+    }
+
+    static Rectangle *make(const std::string &str) {
+        std::stringstream s(str);
+        double height, width;
+        s >> height;
+        s >> width;
+        return new Rectangle(height, width);
+    }
+
+private:
+    double height;
+    double width;
+};
+
+
+class Square : public Figure {
+public:
+
+    explicit Square(double size) : size(size) {}
+
+    double get_square() const final {
+        return size * size;
+    }
+
+    static Square *make(const std::string &str) {
+        std::stringstream s(str);
+        double size;
+        s >> size;
+        return new Square(size);
+    }
+
+private:
+    double size;
+};
+
+
+class Circle : public Figure {
+public:
+    explicit Circle(double radius) : radius(radius) {}
+
+    double get_square() const final {
+        return M_PI * radius * radius;
+    }
+
+    static Circle *make(const std::string &str) {
+        std::stringstream s(str);
+        double rad;
+        s >> rad;
+        return new Circle(rad);
+    }
+
+private:
+    double radius;
+};
