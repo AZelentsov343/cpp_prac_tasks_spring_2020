797afa446a973ca985f635a62a6515e123076ee1
Alex <54078844+AZelentsov343@users.noreply.github.com>
2020-05-16 00:21:06 +0300

    Update 3.cpp

--- 6contest/3.cpp
+++ 6contest/3.cpp
@@ -15,3 +15,34 @@ hexagons
 размеры поля, две координаты клеток, которая вычисляет расстояние между двумя клетками.
 
 
+#include <cmath>
+#include <algorithm>
+
+namespace Game {
+
+template<typename value_type>
+class Coord {
+public:
+    explicit Coord(value_type row = 0, value_type col = 0) : row(row), col(col) {};
+    value_type row, col;
+};
+
+template<typename value_type>
+value_type dist(Coord<value_type> field, Coord<value_type> first, Coord<value_type> second) {
+    value_type distance_square = abs(first.col - second.col);
+    value_type angle_moves = distance_square / 2;
+
+    value_type addition(0);
+
+    if (distance_square % 2 && second.col % 2 && ((second.row > first.row && second.col > first.col)
+    || (second.row > first.row && second.col < first.col))) {
+        ++addition;
+    } else if (distance_square % 2 && first.col % 2 && ((second.row < first.row && second.col < first.col)
+    || (second.row < first.row && second.col > first.col))) {
+        ++addition;
+    }
+
+    value_type rows = abs(first.row - second.row);
+    return distance_square + rows - std::min(rows, angle_moves + addition);
+}
+} //namespace Game
